afe89c4479cbbdb35fbcf229acde46410aad9605
petka61 <petka061@gmail.com>
2022-01-30 16:01:06 +0300

    first task is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,14 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let discriminant = Math.pow(b, 2) - (4*a*c);
+  if (discriminant > 0) {
+    arr.push((-1*b + Math.sqrt(discriminant) )/(2*a));
+    arr.push((-1*b - Math.sqrt(discriminant) )/(2*a));
+  } else if(discriminant == 0) {
+    arr.push((-1*b)/(2*a));
+  } else if (discriminant < 0) {
+    console.log("Дискриминант меньше 0. Корней нет.")
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
