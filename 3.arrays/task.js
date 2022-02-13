905e3832c72406c22c8aebfe7f15630e8ee4a199
petka61 <petka061@gmail.com>
2022-02-14 00:18:07 +0300

    both tasks is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,29 @@
 function compareArrays(arr1, arr2) {
-  let result;
+if (arr1.length != arr2.length) {
+  return false;
+}
+let result = arr1.every((item, index) => item === arr2[index]);
 
-  // Ваш код
-
-  return result; // boolean
+  // let result = function() {
+  //   if (arr1.length == arr2.length) {
+  //     for (var i in arr1) {
+  //     for (var m in arr2) {
+  //       if (arr1[i] === arr2[m]) {
+  //         return true
+  //       } else {
+  //         return false
+  //       }
+  //     }
+  //     }
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  // return result(); // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item, index) => item >= 3 && item%3 == 0).map((item) => item * 10);
   return resultArr; // array
 }
