197b93f152976673e74bd8e38ea46d169e65f898
petka61 <petka061@gmail.com>
2022-03-01 20:47:33 +0300

    exercise fixed

--- 7.async/task.js
+++ 7.async/task.js
@@ -12,47 +12,52 @@ class AlarmClock {
       console.error('This alarm is already created');
       return;
     }
-        this.alarmCollection.push({id, time, callback});
+    this.alarmCollection.push({
+      id,
+      time,
+      callback
+    });
   }
 
 
   removeClock(id) {
     const beforeRemoveClock = this.alarmCollection.length;
     this.alarmCollection = this.alarmCollection.filter(item => item.id !== id);
-    return beforeRemoveClock === this.alarmCollection;
-}
+    return beforeRemoveClock != this.alarmCollection;
+  }
 
   getCurrentFormattedTime() {
-    return new Date().toLocaleTimeString().slice(0,-3);
+    return new Date().toLocaleTimeString().slice(0, -3);
   }
 
   start() {
     function checkClock(alarm) {
       console.log(alarm.time)
-      if(this.getCurrentFormattedTime() == alarm.time) {
-        return alarm.callback;
+      if (this.getCurrentFormattedTime == alarm.time) {
+        alarm.callback();
       }
     }
+    checkClock = checkClock.bind(this.start)
     if (!this.timerId) {
-       setInterval(() => {
-         this.alarmCollection.forEach((alarm) => checkClock(alarm));
-       }, 1000);
+      setInterval(() => {
+        this.alarmCollection.forEach((alarm) => checkClock(alarm));
+      }, 1000);
     }
   }
 
-stop() {
-  if (this.timerId) {
-    clearInterval(this.timerId);
-    this.timerId = null;
-}
-}
+  stop() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
 
-printAlarms() {
-  this.alarmCollection.forEach(item => console.log(`Будильник №${alarm.id} установлен на ${alarm.time}`));
-}
+  printAlarms() {
+    this.alarmCollection.forEach(item => console.log(`Будильник №${alarm.id} установлен на ${alarm.time}`));
+  }
 
-clearAlarms() {
+  clearAlarms() {
     this.stop();
     this.alarmCollection = [];
-};
+  };
 }
